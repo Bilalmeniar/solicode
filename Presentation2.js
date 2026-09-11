51e76d53265551a2bd1d173b0db089a3da76c19a
Bilalmeniar <meniar.bilal313@gmail.com>
2026-09-11 16:39:29 +0100

    Add files via upload

---
+++ Presentation2.js
@@ -0,0 +1,9 @@
+const prompt = require("prompt-sync")();
+let nom = prompt("quel est votre nom ?: ");
+let prénom = prompt("quel est votre prénom ?: ");
+let formation = prompt("quel formation ?: ");
+
+
+console.log("Bonjour", nom);
+console.log(prénom);
+console.log(formation);
