b7a8c7b6f94a314b9beb0a1afb4d0f4b2a76a3c5
Bilalmeniar <meniar.bilal313@gmail.com>
2026-09-14 09:22:10 +0100

    Add age input and update console logs
    Added age prompt and logging to the output.

--- Presentation2.js
+++ Presentation2.js
@@ -1,9 +1,11 @@
 const prompt = require("prompt-sync")();
 let nom = prompt("quel est votre nom ?: ");
 let prénom = prompt("quel est votre prénom ?: ");
+let age = number("quelest votre age ?: ");
 let formation = prompt("quel formation ?: ");
 
 
 console.log("Bonjour", nom);
 console.log(prénom);
+console.log(age);
 console.log(formation);
